bc2cef3aa48bfca461fd4784cd43df540b508eae
alopezna5 <48030109+alopezna5@users.noreply.github.com>
2020-04-19 18:26:47 +0200

    Feat: Alfa 0.1 (#2)
    * feat: resultant tuple  stored in a set
    
    * feat some print improves
    
    * feat: first DB insertion approach

--- email_finder.py
+++ email_finder.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Google search
 from googlesearch import search
+
+# Requests, html and searches utils
 import re
 import requests
 from bs4 import BeautifulSoup
+
+# Database utils
+import sqlite3
+from sqlite3 import Error
 
 TITLE = """
   ______  __  __            _____  _        ______  _____  _   _  _____   ______  _____  
@@ -16,7 +23,7 @@ TITLE = """
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 """
 
-query = "QUERY TO SEARCH"
+query = "Madrid AND restaurante AND email"
 
 print(TITLE)
 
@@ -27,23 +34,62 @@ for i in search(query,  # The query you want to run
                 lang='en',  # The language
                 num=10,  # Number of results per page
                 start=0,  # First result to retrieve
-                stop=100,  # Last result to retrieve
+                stop=40,  # Last result to retrieve
                 pause=2.0,  # Lapse between HTTP requests
                 ):
     my_webs_page_result_list.append(i)
-    print(i)
+print("[!] DONE")
 
 print("[!] Getting the web emails")
+emails_set = set()
+
 for web_page in my_webs_page_result_list:
     try:
+        # Html finder
         r = requests.get(web_page)
         data = r.text
         soup = BeautifulSoup(data, "html.parser")
 
-        for i in soup.find_all(href=re.compile("mailto")):
-            if i.string != None:
-                i.string = i.string.replace("\t", "").replace("\n", "").replace(" ","")
-                print(i.string)
+        # Title finder
+        try:
+            title = soup.title.text
+        except:
+            title = None
 
+        # Email finder
+        first_email_search_filter = soup.find_all(href=re.compile("mailto"))
+        if len(first_email_search_filter) > 1:
+            for i in first_email_search_filter:
+                if i.string != None and "@" in i.string:
+                    i.string = i.string.replace("\t", "").replace("\n", "").replace(" ", "")
+                    email = i.string
+                    emails_set.add((web_page, title, email))
     except:
         pass
+print("[!] DONE")
+
+for email in emails_set:
+    print(email)
+
+# Create a database connection to a SQLite database and insert the elements
+print("[!] Inserting in DB")
+
+conn = None
+try:
+    conn = sqlite3.connect("emails_database.db")  # Create db connection
+    cur = conn.cursor()
+
+    cur.execute(" CREATE TABLE IF NOT EXISTS emails(id integer, web text, title text, email text)")  # Create emails table
+    conn.commit()
+
+    i = 1
+    for email in emails_set:
+        cur.execute("INSERT INTO emails VALUES(?, ?, ?, ?)", (i, email[0], email[1], email[2]))
+        conn.commit()
+        i += 1
+
+except Error as e:
+    print(e)
+finally:
+    if conn:
+        conn.close()
