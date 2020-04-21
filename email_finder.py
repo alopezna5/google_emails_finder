637bf37b46b0c10d35d2a3379a363ce47618a680
Alvaro <alopezgn@gmail.com>
2020-04-21 14:12:48 +0200

    feat: [WIP] several improves and second level search fixes

--- email_finder.py
+++ email_finder.py
@@ -23,6 +23,7 @@ TITLE = """
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
 """
 
+
 def _make_a_google_query(query):
     """
     :param query: The query to make in google
@@ -33,9 +34,9 @@ def _make_a_google_query(query):
     for i in search(query,  # The query you want to run
                     tld='com',  # The top level domain
                     lang='en',  # The language
-                    num=10,  # Number of results per page
+                    num=100,  # Number of results per page
                     start=0,  # First result to retrieve
-                    stop=40,  # Last result to retrieve
+                    stop=500,  # Last result to retrieve
                     pause=2.0,  # Lapse between HTTP requests
                     ):
         my_webs_page_result_list.append(i)
@@ -48,14 +49,19 @@ def _fist_level_email_finder(url, emails_set):
     :param url: The URL where it is going to search
     :return: It return the emails found
     """
+
     # Html finder
-    r = requests.get(url)
+    try:
+        r = requests.get(url)
+    except:
+        return emails_set
+
     data = r.text
     soup = BeautifulSoup(data, "html.parser")
 
     # Title finder
     try:
-        title = soup.title.text
+        title = soup.title.text.replace("\n", "").replace("\t", "")
     except:
         title = None
 
@@ -66,7 +72,17 @@ def _fist_level_email_finder(url, emails_set):
             if i.string != None and "@" in i.string:
                 i.string = i.string.replace("\t", "").replace("\n", "").replace(" ", "")
                 email = i.string
+                print(url, title, email)
                 emails_set.add((url, title, email))
+            else:
+                href_email = i.attrs.get('href', None)
+                if href_email != None and "@" in href_email:
+                    email_string = href_email.replace("\t", "").replace("\n", "").replace(" ", "").replace("mailto:",
+                                                                                                           "").replace("?subject=", "")
+                    email = email_string
+                    print(url, title, email)
+                    emails_set.add((url, title, email))
+    return emails_set
 
 
 def _second_level_email_finder(url, emails_set, pattern):
@@ -75,41 +91,67 @@ def _second_level_email_finder(url, emails_set, pattern):
     :param pattern: The pattern for considering a link relevant for the search
     :return: It go to the found links that contains a given pattern and makes a first_level search in the found link
     """
+    hrefs_set = set()
+
     # Html finder
-    r = requests.get(url)
+    try:
+        r = requests.get(url)
+    except:
+        return emails_set
+
     data = r.text
     soup = BeautifulSoup(data, "html.parser")
 
     second_email_search_filter = soup.find_all(href=re.compile(pattern))
-    if len(second_email_search_filter) > 0:
-        for i in second_email_search_filter:
-            if not "http" in i:
-                _fist_level_email_finder(url + i['href'], emails_set)
+
+    for pattern_match in second_email_search_filter:
+        # Obtained list is inserted in a set for avoiding duplications
+        hrefs_set.add(pattern_match['href'])
+
+    if len(hrefs_set) > 0:
+        for link in hrefs_set:
+            if ".html" in url:
+                url = '/'.join(url.split("/")[:-1])
+
+            if not "http" in link:
+                emails_set = _insert_found_emails(emails_set, _fist_level_email_finder(url + link, emails_set))
             else:
-                _fist_level_email_finder(i['href'], emails_set)
+                emails_set = _insert_found_emails(emails_set, _fist_level_email_finder(link, emails_set))
+
+    return emails_set
+
+
+def _insert_found_emails(emails_set, new_emails_set):
+    for new_email in new_emails_set:
+        print(new_email)
+        emails_set.add(new_email)
+    return emails_set
 
 
 def main():
     print(TITLE)
     emails_set = set()
 
-    query_results = _make_a_google_query("'Colmenar Viejo' AND restaurantes AND email")
-    
+    query_results = _make_a_google_query("'Colmenar Viejo' AND restaurantes")
+
     print("[!] Getting the web emails")
     for web_page in query_results:
-        try:
-            # First level email finder
-            _fist_level_email_finder(web_page, emails_set)
+        # try:
+        # First level email finder
+        emails_set = _insert_found_emails(emails_set, _fist_level_email_finder(web_page, emails_set))
 
-            # Second level email finder if it found more links to what I am looking for
-            _second_level_email_finder(web_page, emails_set, "/Restaurant")
-            _second_level_email_finder(web_page, emails_set, "/restaurant")
+        # Second level email finder if it found more links to what I am looking for
+        emails_set = _insert_found_emails(emails_set, _second_level_email_finder(web_page, emails_set, "/Restaurant"))
+        emails_set = _insert_found_emails(emails_set, _second_level_email_finder(web_page, emails_set, "/restaurant"))
 
-            for email in emails_set:
-                print(email)
-        except:
-            pass
-    print("[!] DONE")
+        # except:
+        #     pass
+
+
+    for email in emails_set:
+        print(email)
+
+    print("[!] DONE, {} emails found".format(len(emails_set)))
 
     # Create a database connection to a SQLite database and insert the elements
     print("[!] Inserting in DB")
